ef086908b99bdef8e38f667c9ac1cb2a04079b62
Natas Pijoraitis <nataspijoraitis@gmail.com>
2025-09-25 13:52:21 +0200

    feat: navigation

---
+++ workshops/template/contact.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Klantenbron</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Klantenbron</h1>
+    <nav>
+    <?php
+    include 'includes/nav.php';
+    ?>
+    </nav>
+</body>
+</html>
---
+++ workshops/template/includes/nav.php
@@ -0,0 +1,6 @@
+<nav>
+<li><a href="index.php">Home</a></li>
+<li><a href="about.php">About</a></li>
+<li><a href="contact.php">Contact</a></li>
+<li><a href="news.php">News</a></li>
+</nav>
